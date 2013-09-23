9ccbdf22e8b25c5bff08300bf22d8db745c96718
verphen <verphen@163.com>
2013-09-23 14:25:02 +0800

    init commit

--- Test.java
+++
@@ -1,7 +0,0 @@
-package org.verphen.test
-
-public class Test{
-  public static void mian(String args[]){
-    System.out.println("Hello,Web Service !");
-  }
-}
---
+++ src/org/verphen/test/MainDemo.java
@@ -0,0 +1,13 @@
+/**
+ * @author VerpHen
+ * @date 2013年9月23日  下午2:23:50
+ */
+
+package org.verphen.test;
+
+public class MainDemo {
+	public static void main(String[] args) {
+		System.out.println("--------------------");
+	}
+
+}
